57f9ed7e731f56af405979fdff55cb4d04faf1ef
Miljan Mijatovic <miljanmijatovic322@gmail.com>
2026-07-01 16:36:32 +0200

    Definisane belongsTo relacije i fillable polja unutar Favorite modela

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
-    //
+    use HasFactory;
+
+    // Dozvoljavamo masovno dodavanje za kljucna polja (zahtev za stabilnost)
+    protected $fillable = ['user_id', 'movie_id'];
+
+    // Relacija: Svaki zapis u tabeli favorites pripada jednom korisniku
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relacija: Svaki zapis u tabeli favorites pripada jednom filmu
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
