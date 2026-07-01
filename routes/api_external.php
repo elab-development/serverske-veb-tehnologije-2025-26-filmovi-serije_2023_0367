a02d0ef19c61c3d8e4b3274aa6cb93bda532b98d
Miljan Mijatovic <miljanmijatovic322@gmail.com>
2026-07-01 13:05:45 +0200

    Dodate rute za eksterni API u routes/api_external.php

--- routes/api_external.php
+++ routes/api_external.php
@@ -1,2 +1,10 @@
 <?php
-// Ovde pišeš svoje rute
+
+use App\Http\Controllers\ExternalApiController;
+use Illuminate\Support\Facades\Route;
+
+// Ruta za pretragu filmova preko OMDB servisa
+Route::get('/external/omdb', [ExternalApiController::class, 'searchOmdb']);
+
+// Ruta za dobijanje popularnih filmova preko TMDB servisa
+Route::get('/external/tmdb/popular', [ExternalApiController::class, 'getPopularTmdb']);
