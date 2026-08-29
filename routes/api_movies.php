88935f55f3ca87c846e7b8c58040f5dca0e97dfd
Aleksa Randjelovic <randjelovicaleksa04@gmail.com>
2026-08-29 18:46:41 +0200

    Popravljen api_movies_php

--- routes/api_movies.php
+++ routes/api_movies.php
@@ -1,18 +1,21 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MoviePosterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('movies/stats/genres', [MovieController::class, 'genreStats']);
 Route::get('movies/top-rated', [MovieController::class, 'topRated']);
 Route::get('genres', [MovieController::class, 'indexGenres']);
 
-Route::apiResource('movies', MovieController::class)->only(['index', 'show']);
+Route::get('movies', [MovieController::class, 'index']);
+Route::get('movies/{id}', [MovieController::class, 'show']);
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::post('movies', [MovieController::class, 'store']);
     Route::put('movies/{id}', [MovieController::class, 'update']);
     Route::delete('movies/{id}', [MovieController::class, 'destroy']);
+    Route::post('movies/upload-poster', [MoviePosterController::class, 'uploadPoster']);
 
     Route::post('genres', [MovieController::class, 'storeGenre']);
     Route::put('genres/{id}', [MovieController::class, 'updateGenre']);
